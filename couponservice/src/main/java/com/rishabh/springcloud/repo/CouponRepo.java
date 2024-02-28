7bd7c6e64e794d5df06283ff988187f36cb3e1d8
Rishabh Sharma <rishabhsharma.5554@gmail.com>
2024-02-28 23:06:36 +0530

    Inital Commit for API Gateway

---
+++ couponservice/src/main/java/com/rishabh/springcloud/repo/CouponRepo.java
@@ -0,0 +1,8 @@
+package com.rishabh.springcloud.repo;
+
+import com.rishabh.springcloud.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepo extends JpaRepository<Coupon,Long> {
+    Coupon findByCode(String code);
+}
